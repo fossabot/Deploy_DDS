beef1ad56c51d18db6d6fd65ab0eb6f3e8d72a1b
pragneshrana <pragneshrana244@gmail.com>
2021-06-14 21:07:43 +0530

    code changes

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,50 +1,65 @@
 #updating
+echo 'Updating System'
 sudo apt-get update
 
 #install python
-sudo apt-get install python3
+echo 'Installing Python'
+sudo apt-get install python3.6
 
 #install RDkit
+echo 'Installing RDKIT'
 sudo apt-get install python3-rdkit
 conda install -c rdkit rdkit
 #conda install -c conda-forge rdkit
 
-#numpy and scipy
-sudo apt install python3-numpy python3-scipy
+#numpy
+echo 'Installing NUMPY'
+pip install numpy
+
+#scipy
+echo 'Installing SCIPY'
+pip install scipy
 
 #matplotlib
-sudo apt-get install python3-matplotlib
+echo 'Installing MATPLOTLIB'
+pip install matplotlib
 
 #pandas
-sudo apt-get install python3-pandas
-sudo pip install --upgrade pandas
+echo 'Installing PANDAS'
+pip install pandas
 
 #regex
-sudo apt-get install python3-regex
+echo 'Installing REGEX'
+pip install regex
 
 #statsModel
+echo 'Installing STATMODELS'
 pip install statsmodels
-#conda install statsmodels
 
 #collection
+echo 'Installing collections-libs'
 pip install collections-extended
 
 #random
+echo 'Installing RANDOM'
 pip install random2
 
 #seaborn
-pip3 install seaborn
+echo 'Installing SEABORN'
+pip install seaborn
 
 #sklearn
-pip3 install sklearn
+echo 'Installing SKLEARN'
+pip install -U scikit-learn
 
 #coverage
+echo 'Installing COVERALL'
 pip install coveralls
 
 #codecov
+echo 'Installing CODECOVERAGE'
 pip install codecov
 
 #latex
-sudo pip install latex
-sudo apt-get install dvipng
-sudo apt-get install -y texlive texlive-latex-extra texlive-latex-recommended
+echo 'Installing texlive'
+sudo apt-get install texlive-full
